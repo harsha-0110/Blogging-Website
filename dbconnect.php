b5e2734e24a25365a0af0b85777b8081552a237c
harsha-0110 <112392323+harsha-0110@users.noreply.github.com>
2023-02-24 13:50:08 +0530

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "blog2";
+$servername = "db_servername";
+$username = "db_username";
+$password = "db_password";
+$dbname = "db_name";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
