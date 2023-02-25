e98b48e86652233091da893142139d9aa97bcc64
harsha-0110 <112392323+harsha-0110@users.noreply.github.com>
2023-02-25 09:37:39 +0530

    Update header.php

--- header.php
+++ header.php
@@ -3,8 +3,8 @@
 <head>
   <title>BloggIT</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" type="text/css" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" href="style.css">
   <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
   <script>
     tinymce.init({
