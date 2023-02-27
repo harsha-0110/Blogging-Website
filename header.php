04d403690538a33d21ae85ec81d8c9b46830779b
harsha-0110 <112392323+harsha-0110@users.noreply.github.com>
2023-02-27 14:21:19 +0530

    Update header.php

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html>
 <head>
   <title>BloggIT</title>
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" type="text/css" href="https://www.w3schools.com/w3css/4/w3.css">
   <link rel="stylesheet" href="style.css">
