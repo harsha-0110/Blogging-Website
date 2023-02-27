e5675a1631ab36ee07352c8c30541fa05aaa01fb
harsha-0110 <112392323+harsha-0110@users.noreply.github.com>
2023-02-27 14:19:58 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -41,6 +41,7 @@ if(isset($_POST["login"])){
 <html>
 <head>
     <title>BloggIT</title>
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" type="text/css" href="https://www.w3schools.com/w3css/4/w3.css">
